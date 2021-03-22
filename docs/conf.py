d2fed049a8ed08f0d906f8872767f6131b895e8c
ricardofrantz <rasfrantz@gmail.com>
2021-03-22 14:25:33 +0100

    update

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ fortran_ext = ['*.f'] #, 'f90', 'F90', 'f95', 'F95']
 # have an extension matching those define by the config variable `fortran_ext` are used.
 
 # import os, sys
-fortran_src = [ os.path.abspath('../core'),  ]
+fortran_src = [ os.path.abspath('../core/'),  ]
 #fortran_encoding = ["utf8"]
 #fortran_subsection_type = ["rubric"]
 # # "rubric" (default) : use directive rubric (lightweight title in bold).
